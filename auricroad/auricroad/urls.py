f992bdcfb3c4223120c61c5dc56a753a012a30a8
Jacob Kent <kentjakel@wustl.edu>
2019-04-11 14:38:53 -0400

    get wagtail set up, add customimage class

--- auricroad/auricroad/urls.py
+++ auricroad/auricroad/urls.py
@@ -4,6 +4,10 @@ from django.urls import include, path
 from django.views.generic.base import RedirectView
 from django.views.static import serve as static_serve
 
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.core import urls as wagtail_urls
+
 urlpatterns = [
     path(
         r"favicon.ico",
@@ -11,8 +15,11 @@ urlpatterns = [
     ),
     path(r"account/", include("auricroad.account.urls")),
     path(r"admin/", admin.site.urls),
+    path(r"cms/", include(wagtailadmin_urls)),
+    path(r"documents/", include(wagtaildocs_urls)),
     path("", include("social_django.urls", namespace="social")),
     path(r"", include("auricroad.home.urls")),
+    path(r"", include(wagtail_urls)),
 ]
 
 if settings.DEBUG:
