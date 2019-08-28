327a02888693dc2355a9efbddd335c220ce08e7e
Jake Kent <jake-kent@users.noreply.github.com>
2019-08-28 13:50:43 -0400

    add underline option to richtext editor (#30)

--- auricroad/home/wagtail_hooks.py
+++ auricroad/home/wagtail_hooks.py
@@ -1,8 +1,12 @@
 from django.db import models  # NOQA
 from django.utils.translation import ugettext_lazy as _
 
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
+from wagtail.admin.rich_text.converters.html_to_contentstate import \
+    InlineStyleElementHandler
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 from wagtail.contrib.settings.models import BaseSetting, register_setting
+from wagtail.core import hooks
 
 from .models import Contact, Footer, Hotel, NavBar
 
@@ -74,3 +78,34 @@ class ContactFormSettings(BaseSetting):
         verbose_name=_("from address"), max_length=255, blank=True
     )
     subject = models.CharField(verbose_name=_("subject"), max_length=255, blank=True)
+
+
+@hooks.register("register_rich_text_features")
+def register_strikethrough_feature(features):
+    """
+    Registering the `strikethrough` feature, which uses the `STRIKETHROUGH` Draft.js inline style type,
+    and is stored as HTML with an `<s>` tag.
+    """
+    feature_name = "underline"
+    type_ = "UNDERLINE"
+    tag = "u"
+
+    control = {
+        "type": type_,
+        "label": "U",
+        "description": "Underline",
+        "style": {"textDecoration": "underline"},
+    }
+
+    features.register_editor_plugin(
+        "draftail", feature_name, draftail_features.InlineStyleFeature(control)
+    )
+
+    db_conversion = {
+        "from_database_format": {tag: InlineStyleElementHandler(type_)},
+        "to_database_format": {"style_map": {type_: tag}},
+    }
+
+    features.register_converter_rule("contentstate", feature_name, db_conversion)
+
+    features.default_features.append("underline")
