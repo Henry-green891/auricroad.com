52af03df9037ee616d9817233cdf859b8901a056
Ian Pollack <ianjpollack@gmail.com>
2022-04-13 11:01:14 -0700

    replaced issing urls

--- auricroad/auricroad/urls.py
+++ auricroad/auricroad/urls.py
@@ -20,7 +20,8 @@ urlpatterns = [
     path(r"cms/", include(wagtailadmin_urls)),
     path(r"documents/", include(wagtaildocs_urls)),
     path("", include("social_django.urls", namespace="social")),
-
+    path(r"", include("auricroad.home.urls")),
+    path(r"", include(wagtail_urls)),
 ]
 
 handler404 = home_views.handler404
